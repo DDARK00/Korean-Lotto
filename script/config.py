b1778985d1769578991e3eb6f8ce97239d46b0c0
DDARK <156265425+DDARK00@users.noreply.github.com>
2026-05-24 17:11:13 +0900

    fix: fix header_path from processor.py

--- script/config.py
+++ script/config.py
@@ -7,3 +7,4 @@ FE_DATA_PATH = BASE_DIR / "fe" / "public" / "data" / "lotto_history.json"
 WASM_OUTPUT_PATH = BASE_DIR / "fe" / "public" / "wasm" / "engine.js"
 WASM_CPP_PATH = BASE_DIR / "wasm" / "src"
 EMSDK_ENV_PATH = BASE_DIR / "emsdk" / "emsdk_env.bat" # 혹은 .sh
+HEADER_PATH = BASE_DIR / "wasm" / "src" / "lotto_data.h"
