6c71ec7988ed70ec7f37cbef8d2f8e7adfc51ab3
DDARK <156265425+DDARK00@users.noreply.github.com>
2026-06-10 17:16:50 +0900

    feat: gh-pages ci/cd

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     assetsInlineLimit:0, // 너무 작은 WASM 파일이 base64 문자열로 인라인화되는 것을 방지 (0으로 설정)
   },
   assetsInclude: ['**/*.wasm'], // WASM 파일을 에셋으로 명확히 인식
+  base:'/Korean-Lotto/'
   // server: {
   //   port: 3000,
   // },
